66c67d06f533cb2bf1e701191746230cac93c4c1
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2021-04-27 09:09:36 +0000

    make other page and do more scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: 'home#index'
+
+
+get '/other' , controller: "home" , action: "other"
+
+
+
+  root to: 'home#index'
 
 end

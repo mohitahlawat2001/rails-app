a9d7fbf113190cca5348cbdc00ea309808e16674
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2021-04-30 17:15:23 +0000

    add comment box

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery
+  skip_before_action :verify_authenticity_token
+
+
+    def current_user
+      session_user_id = session[:user_id]
+      User.find_by(id: session_user_id)
+    end
+
+    helper_method :current_user
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,14 @@ class CommentsController < ApplicationController
     def index
         @comments = Comment.all
     end
+
+    def create
+        body = params[:comment][:body]
+        user_id = current_user.id
+
+
+        Comment.create(body: body ,user_id: user_id)
+    
+        redirect_to :comments
+    end
 end

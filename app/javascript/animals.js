651f10eb44e0ccf5758dfae4ece045fbf1c67f62
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2021-05-07 16:27:44 +0000

    added clousers

--- app/javascript/animals.js
+++ app/javascript/animals.js
@@ -1,23 +1,23 @@
 
 
-function whatTodoWhenTheJSONComeBack(data) {
-    let sound = data.sound;
-    console.log('the animal say:', sound);
-}
-
-function whatTodo(response) {
-    response.json().then(whatTodoWhenTheJSONComeBack);
-}
 
 
 
-function handleClick(clickEvent) {
+
+
+
+
+
+
+document.addEventListener('click', (clickEvent) => {
     let clickedElement = clickEvent.target;
     let clickedNumber = clickedElement.innerText;
        console.log('clicked',clickedNumber);
 
-    fetch('/animals/' + clickedNumber).then(whatTodo)
-}
-
-
-document.addEventListener('click', handleClick);
+    fetch('/animals/' + clickedNumber).then((response) => {
+        response.json().then((data) => {
+            let sound = data.sound;
+            console.log('the animal say:', sound);
+        });
+    });
+});
